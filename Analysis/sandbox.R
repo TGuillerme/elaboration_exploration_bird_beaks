1360a13134233beab23e6c8738f8cfd504758738
TGuillerme <guillert@tcd.ie>
2021-04-06 19:15:37 +0100

    TODO

---
+++ Analysis/sandbox.R
@@ -0,0 +1,9 @@
+
+#TODO:
+list_of_matrices <- get.covar(data, n)
+plot.ellipses(list_of_matrices)
+
+compare.matrices() # comparing the VCV matrices Robinson and Beckerman style
+
+get.major.axis.coords(list_of_matrices, axis = 1) # Getting the list of major axis (get the eigen value from it?)
+
