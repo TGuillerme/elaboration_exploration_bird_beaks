b539416e99d2ff665bf750e4bcb0346f2899e241
TGuillerme <guillert@tcd.ie>
2021-04-06 17:13:08 +0100

    TODOs

--- Analysis/get.ellipse.R
+++ Analysis/get.ellipse.R
@@ -36,3 +36,13 @@ my_fun <- function() {
 
     return()
 }
+
+#TODO:
+list_of_matrices <- get.covar(data, n)
+plot.ellipses(list_of_matrices)
+
+compare.matrices() # comparing the VCV matrices Robinson and Beckerman style
+
+get.major.axis.coords(list_of_matrices, axis = 1) # Getting the list of major axis
+
+
