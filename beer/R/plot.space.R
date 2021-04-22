e8bfef0f0acb272eb77bc91f794f7221474d56ea
TGuillerme <guillert@tcd.ie>
2021-04-22 16:48:41 +0100

    Updated packages (not all tested

--- beer/R/plot.space.R
+++ beer/R/plot.space.R
@@ -2,12 +2,17 @@
 #'
 #' @description Plotting axes from an ellipse
 #'
-#' @param beer the \code{"beer"} object.
+#' @param data a trait space.
+#' @param levels the levels (categories, eg. clades)
 #' @param dimensions which dimensions to plot (default is \code{c(1,2)}).
 #' @param ... any plotting options to be passed to \code{\link{graphics}{lines}}
+#' @param scale.axes logical, whether to set both axes on the same scale (\code{TRUE}) or not (\code{FALSE}, default).
 #' @param add logical, whether to add the lines to an existing plot (\code{TRUE}, default) or not (\code{FALSE}).
 #' 
 #' @examples
+#' data(morphdat)
+#' data <- morphdat[, -c(4,5)]
+#' plot.space(data, levels = morphdat$clade)
 #'
 #' @seealso
 #' 
@@ -15,7 +20,41 @@
 #' @importFrom graphics lines
 #' @export
 
-plot.space <- function(beer, dimensions, ..., add) {
-    # graphics::lines(1,1)
-    return("prototype")
+plot.space <- function(data, levels, dimensions = c(1,2), ..., col, add = FALSE, legend, scale.axes = FALSE) {
+
+    ## Get the plotting arguments
+    if(!scale.axes) {
+        plot_args <- plot.dots(dimensions = c(1,2), ...)
+    } else {
+        plot_args <- plot.dots(data = data,dimensions = c(1,2), ...)
+    }
+    
+    if(missing(col)) {
+        ## Set the default colours
+        if(missing(levels)) {
+            col <- "black"
+
+        } else {
+            col <- grDevices::rainbow(length(unique(levels)))
+        }
+    }
+    ## Assigning the colour (by levels or not)
+    plot_args$col <- col
+    if(!missing(levels)) {
+        if(length(col) != length(levels)) {
+            plot_args$col <- col[as.factor(levels)]
+        }
+    }
+
+    ## Adding the data
+    plot_args$x <- data[, dimensions[1]]
+    plot_args$y <- data[, dimensions[2]]
+
+    ## Plotting the results
+    if(!add) {
+        do.call(plot, plot_args)
+    } else {
+        do.call(points, plot_args)
+    }
+    return(invisible())
 }
--- beer/tests/testthat/test-plot.space.R
+++ beer/tests/testthat/test-plot.space.R
@@ -1,4 +1,8 @@
 ## Test
 test_that("plot.space works", {
-    expect_equal(plot.space(), "prototype")
+    data(morphdat)
+    my_data <- morphdat[, -c(4,5)]
+    my_levels <- morphdat$clade
+    expect_null(plot.space(my_data, pch = 21, cex = 1.5))
+    expect_null(plot.space(my_data, levels = my_levels, add = TRUE))
 })
