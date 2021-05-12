0a412a00af2943ff72d0f93aeca19164e7516949
TGuillerme <guillert@tcd.ie>
2021-05-12 15:17:40 +0100

    optimised replicates function

--- beer/R/optim.replicate.R
+++ beer/R/optim.replicate.R
@@ -2,10 +2,11 @@
 #'
 #' @description A wrapper for replicating a based on results variance
 #'
-#' @param function The function to execute
+#' @param fun The function to execute. The function should output a single value or a vector or a list of values.
+#' @param diagnose The diagnosis function should intake a vector of values.
+#' @param summarise Optional, if the output of \code{fun()} is complex, a function to summarise that output and pass it to \code{diagnose()}.
 #' @param minimum The minimum of replicates to run (default is 20)
 #' @param maximum Optional, a maximum of iterations (default is Inf).
-#' @param diagnose The diagnosis function
 #' @param stop.variance The percentage of variance change for stopping (default is 0.05).
 #' @param increment The percentage of increment (if left empty, the value from stop.variance is used).
 #' @param verbose Whether to be verbose (\code{TRUE}) or not (\code{FALSE}; default)
@@ -13,62 +14,119 @@
 #' @param par.plan If \code{parallel = TRUE}, which plan to use (see \code{\link[future]{plan}}; by default, the plan is "future::multisession").
 #' @param ... Any optional arguments to be passed to the function.
 #' 
+#' @return
+#' A list of:
+#' \itemize{
+#'      \item{results}: a \code{matrix} with the summarised output from \code{fun()} for every set of iterations;
+#'      \item{diagnoses}: a \code{matrix} with changes in variances after each diagnosis;
+#'      \item{outputs}: a \code{list} of the outputs of each iteration of \code{fun()}.
+#' }
+#' 
 #' @examples
 #'
 #' @seealso
 #' 
 #' @author Thomas Guillerme
 #' @export
+optim.replicate <- function(fun, diagnose, summarise, minimum = 20, maximum = Inf, stop.variance = 0.05, increment, verbose = FALSE, parallel = FALSE, par.plan = "future::multisession", ...) {
 
-# optim.replicate <- function(function, diagnose, minimum = 20, maximum = Inf, stop.variance = 0.05, increment, verbose = FALSE, parallel = FALSE, par.plan = "future::multisession", ...) {
+    ## Check increment
+    if(missing(increment)) {
+        increment <- stop.variance
+    }
 
-#     ## Check increment
-#     if(missing(increment)) {
-#         increment <- stop.variance
-#     }
+    ## Check diagnosis
+    if(missing(diagnose)) {
+        stop("Impossible to optimize the replicate loop without a diagnose function.")
+    }
 
-#     ## Check diagnosis
-#     if(missing(diagnose)) {
-#         stop("Impossible to optimize the replicate loop without a diagnose function.")
-#     }
+    ## Check summary
+    if(missing(summarise)) {
+        summarise <- function(x) return(x)
+    }
 
-#     ## Set the parallel (or not)
-#     if(parallel) {
-#         ## Set the plan for the future (please switch to: capitalism = NULL for a brighter future)
-#         future::plan(par.plan)
-#         ## Set the replicate function type
-#         rep.fun <- future.apply::future_replicate
-#     } else {
-#         rep.fun <- base::replicate
-#     }
+    ## Set the parallel (or not)
+    if(parallel) {
+        ## Set the plan for the future (please switch to: capitalism = NULL for a brighter future)
+        future::plan(par.plan)
+        ## Set the replicate function type
+        rep.fun <- future.apply::future_replicate
+    } else {
+        rep.fun <- base::replicate
+    }
 
-#     ## Set the function to be verbose or not
-#     eval.verbose(fun, )
+    ## Set the function to be verbose or not
+    if(verbose) {
+        ## Set the function to be verbose
+        run.fun <- eval.verbose(fun)
+    } else {
+        run.fun <- fun
+    }
 
+    ## Running the minimum number of iterations
+    if(verbose) {
+        message(paste0("Running the initial ", minimum, " replicates:"), appendLF = FALSE)
+    }
+    output <- rep.fun(minimum, run.fun(), ..., simplify = FALSE)
+    # output <- rep.fun(minimum, run.fun(), simplify = FALSE) ; warning("DEBUG")
+    
+    ## Saving the output and the number of iterations
+    output_save <- output
+    n_iterations <- minimum
 
-#     ## Running the minimum number of iterations
-#     output <- rep.fun(minimum, function, ...)
-#     n_iterations <- minimum
+    ## Summarizing the results
+    results_table <- do.call(rbind, lapply(output, summarise))
 
-#     ## Diagnosing the output variance
-#     diagnosis <- lapply(output, diagnose)
-#     diagnosis_table <- do.call(cbind, diagnosis)
+    ## Iterations done
+    if(verbose) {
+        message("Done.", appendLF = FALSE)
+    }
 
-#     ## Continue the runs
-#     while(!all(apply(diagnosis_table, 2, var) < stop.variance) || n_iterations >= maximum) {
-#         ## Add some iterations
-#         add_iterations <- ceiling(minimum*increment)
-#         output <- unlist(c(output, rep.fun(add_iterations, function, ...), recursive = FALSE))
-#         n_iterations <- n_iterations + add_iterations
+    ## Diagnosing the output variance
+    diagnosis <- apply(results_table, 2, diagnose)
+    diagnosis <- rbind(diagnosis, rep(0, length(diagnosis)))
+    rownames(diagnosis) <- c(minimum, 0)
 
-#         ## Diagnosing the output variance
-#         diagnosis <- lapply(output, diagnose)
-#         diagnosis_table <- do.call(cbind, diagnosis)
-#     }
+    ## Continue the runs if any of the diagnoses has not reached the correct variance
+    while(any(!abs(diagnosis[1,]/diagnosis[2,] - 1) < stop.variance) || n_iterations >= maximum) {
+        
+        ## Add some iterations
+        add_iterations <- ceiling(n_iterations*increment)
 
-#     ## Return the output
-#     return(output)
-# }
+        if(verbose) {
+            message(paste0("\nRunning an additional ", add_iterations, " replicates:"), appendLF = FALSE)
+        }
+        output <- rep.fun(add_iterations, run.fun(), ..., simplify = FALSE)
+        # output <- rep.fun(add_iterations, run.fun(), simplify = FALSE) ; warning("DEBUG")
+
+        ## Saving the output
+        output_save <- c(output_save, output)
+        n_iterations <- n_iterations + add_iterations
+
+        ## Summarizing the results
+        results_table <- rbind(results_table, do.call(rbind, lapply(output, summarise)))
+        
+        if(verbose) {
+            message("Done.", appendLF = FALSE)
+        }
+    
+        ## Diagnosing the output variance
+        new_diagnosis <- apply(results_table, 2, diagnose)
+        diagnosis <- rbind(new_diagnosis, diagnosis)
+        rownames(diagnosis)[1] <- n_iterations
+
+        if(verbose) {
+            message(paste0("\nDiagnosis change: ", paste0(round((diagnosis[2,]/diagnosis[1,]) - 1, digits = 3), collapse = ", ")), appendLF = FALSE)
+        }
+    }
+
+    if(verbose) {
+        message(paste0("\nResults converged after ", n_iterations, " iterations: additional variances (", paste0(round((diagnosis[2,]/diagnosis[1,]) - 1, digits = 3), collapse = ", "), ") all < ", stop.variance, "."), appendLF = FALSE)
+    }
+
+    ## Return the output
+    return(list("results" = results_table, "diagnoses" = diagnosis[rev(1:nrow(diagnosis)), , drop = FALSE], "outputs" = output_save))
+}
 
 eval.verbose <- function(fun, msg = ".") {
     new.fun <- function() {
--- beer/tests/testthat/test-optim.replicate.R
+++ beer/tests/testthat/test-optim.replicate.R
@@ -16,6 +16,45 @@ test_that("verbose function works", {
 })
 
 test_that("optim.replicate works", {
+    set.seed(1)
+    fun1 <- function() return(sample(1:100, 1))
+    fun2 <- function() return(c("a" = sample(1:100, 1), "b" = sample(1:100, 1)))
+    diagnose.fun <- function(x) var(x)
 
+    ## Verbose test
+    messages <- capture_messages(test <- optim.replicate(fun2, diagnose.fun, minimum = 100, verbose = TRUE, stop.variance = 0.001, increment = .05))
+    expect_equal(length(messages), 1518)
+    expect_equal(messages[[1]], "Running the initial 100 replicates:")
+    expect_equal(messages[[2]], ".")
+    expect_equal(messages[[102]], "Done.")
+    expect_equal(messages[[103]], "\nRunning an additional 5 replicates:")
+    expect_equal(messages[[104]], ".")
+    expect_equal(messages[[110]], "\nDiagnosis change: 0.011, -0.022")
+    expect_equal(messages[[111]], "\nRunning an additional 6 replicates:")
+    expect_equal(messages[[1518]], "\nResults converged after 1359 iterations: additional variances (-0.001, 0) all < 0.001.")
+    expect_equal(length(test$outputs), 1359)
+    expect_equal(dim(test$results), c(1359, 2))
+    expect_equal(dim(test$diagnoses), c(54, 2))
 
+    ## Test with a simple function
+    set.seed(1)
+    test <- optim.replicate(fun1, diagnose.fun)
+    expect_equal(length(test$outputs), 21)
+
+    ## Test with a function that outputs two terms
+    set.seed(23)
+    test <- optim.replicate(fun2, diagnose.fun)
+    expect_equal(length(test$outputs), 23)
+
+    ## Test with a function thaty outputs a list (summary function needed)
+    set.seed(42)
+    fun3 <- function() return(list("a" = sample(1:100, 1), "b" = sample(1:100, 1), "c" = sample(1:10000, 1)))
+    sum.fun <- function(x) return(c(x$a, x$b))
+    ## Error
+    expect_error(test <- optim.replicate(fun3, diagnose.fun))
+    ## Works!
+    test <- optim.replicate(fun3, diagnose.fun, summarise = sum.fun)
+    expect_equal(length(test$outputs), 21)
+    expect_equal(dim(test$results), c(21, 2))
+    expect_equal(length(test$outputs[[1]]), 3)
 })
