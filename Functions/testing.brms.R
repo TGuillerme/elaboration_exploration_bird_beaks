aecbf5acf3b057c8e6e6e6cd012629119dc77133
TGuillerme <guillert@tcd.ie>
2021-07-16 16:24:19 +0100

    Update testing.brms.R

--- Functions/testing.brms.R
+++ Functions/testing.brms.R
@@ -1,82 +1,121 @@
-set.seed(42)
-## Loading beer
-library(beer)
-library(MCMCglmm)
+## Loading packages
+library(brms)
 
 ## Loading the demo data
 data(demo_data)
 data(demo_trees)
 data(demo_tree)
 
-## Setting the consensus tree
-consensus_tree <- demo_tree
+######
+# Basic phylo demo from brms
+# https://paul-buerkner.github.io/brms/articles/brms_phylogenetics.html
+######
 
-## Params
-data         = demo_data
-dimensions   = c(1:4)
-tree         = consensus_tree
-trait.family = "gaussian"
-randoms      = c("global", "clade")
-residuals    = "global"
-priors       = 0.02
-verbose      = TRUE
-parameters   = list(nitt   = 10000, thin   = 100, burnin = 0)
-
-
-### bmrs DEMO
-
-
-library(brms)
-## Phylo version
+## Loading demo data
 phylo <- ape::read.nexus("https://paul-buerkner.github.io/data/phylo.nex")
 data_simple <- read.table(
   "https://paul-buerkner.github.io/data/data_simple.txt", 
   header = TRUE
 )
 
-## Set up the clades
+## Creating some clade to test the clade effect
 clade_a <- extract.clade(phylo, node = 327)
 
-## Get the vcv matrix
+## Get the vcv matrix for the tree
 vcv_full_tree <- ape::vcv.phylo(phylo)
 
-## Update the data
+## Creating a clade column (species names)
 clade <- data_simple$phylo
+## Creating a clade_levels column (whether species are in clade 1 or 2)
 clade_levels <- data_simple$phylo %in% clade_a$tip.label + 1
-third_variable <- runif(nrow(data_simple$phylo))
+## Creating a random third variable for fun
+third_variable <- runif(nrow(data_simple))
+## Combining all that together
 data_simple <- cbind(var = third_variable, data_simple, clade, clade_levels)
+## Pouf!
 head(data_simple)
 
-## Simple model (one clade)
+## Simple model from the example: univariate + phylogenetic random effect
 model_simple <- brm(
+  phen ~ cofactor + (1|gr(phylo, cov = vcv_full_tree)), 
+  data = data_simple, 
+  family = gaussian(), 
+  data2 = list(vcv_full_tree = vcv_full_tree),
+  chains = 1,
+  iter = 1000
+)
+
+#######
+## Same model but with an additional clade random effect
+## univariate + phylogenetic random effect + clade level phylogenetic random effect
+#######
+# The idea here is to just add a clade effect using the clade column (species names) with the tree vcv structure and sorting them by clade.
+#
+# Does this measures a separate random phylogenetic effect per clade??
+#
+######
+model_clade <- brm(
   phen ~ cofactor + (1|gr(clade, cov = vcv_full_tree, by = clade_levels)) + (1|gr(phylo, cov = vcv_full_tree)), 
   data = data_simple, 
   family = gaussian(), 
   data2 = list(vcv_full_tree = vcv_full_tree),
-  chains = 2,
-  iter = 1000,
-  prior = c(
-    prior(normal(0, 10), "b"),
-    prior(normal(0, 50), "Intercept"),
-    prior(student_t(3, 0, 20), "sd"),
-    prior(student_t(3, 0, 20), "sigma")
-  )
+  chains = 1,
+  iter = 1000
 )
 
-## Multivariate model (one clade)
-# model_simple <- brm(
-#   mvbind(var, phen, cofactor) ~ clade-1 + (1|gr(clade, cov = vcv_full_tree, by = clade_levels)) + (1|gr(phylo, cov = vcv_full_tree)), 
-#   data = data_simple, 
-#   family = gaussian(), 
-#   data2 = list(vcv_full_tree = vcv_full_tree),
-#   chains = 2,
-#   iter = 1000
-# )
+
+#######
+## Same model but with a multivariate repsonse
+## multivariate + phylogenetic random effect + clade level phylogenetic random effect
+#######
+# The idea here is to make the response variable multivariate following this tutorial
+# https://paul-buerkner.github.io/brms/articles/brms_multivariate.html
+#
+# Does this measures a separate random phylogenetic effect per clade on the three data variables (var, phen and cofactor)?
+#
+######
+
+# Multivariate model (one clade)
+model_clade_multi <- brm(
+  mvbind(var, phen, cofactor) ~ clade + (1|gr(clade, cov = vcv_full_tree, by = clade_levels)) + (1|gr(phylo, cov = vcv_full_tree)), 
+  data = data_simple, 
+  family = gaussian(), 
+  data2 = list(vcv_full_tree = vcv_full_tree),
+  chains = 1,
+  iter = 1000
+)
 
 
-### MODEL COMPARE
+## You can install "beer" manually from the attached zip file using the following
+#install.packages("beer_0.0.3.tar.gz", repos = NULL)
+library(beer) 
 
+## Preparing the demo data
+vcv_full_tree <- ape::vcv.phylo(demo_tree)
+demo_data <- cbind(demo_data, "clade_sp" = demo_data$animal)
 
+#######
+## The multivariate model with the demo data
+## multivariate + phylogenetic random effect + clade level phylogenetic random effect
+#######
+# This is the same as "model_clade_multi" but applied on my own demo data
+#
+######
+brms_model <- brm(
+  mvbind(PC1, PC2, PC3) ~ clade + (1|gr(clade_sp, cov = vcv_full_tree, by = clade)) + (1|gr(animal, cov = vcv_full_tree)), 
+  data = demo_data, 
+  family = gaussian(), 
+  data2 = list(vcv_full_tree = vcv_full_tree),
+  chains = 1,
+  iter = 1000
+)
+
+#######
+## Comparing this to MCMCglmm
+#######
+library(MCMCglmm)
+
+## MCMCglmm priors
 priorRG5 <- list(R=list(
       R1 = list(V = diag(3), nu = 0.002)), 
     G=list(
@@ -84,34 +123,14 @@ priorRG5 <- list(R=list(
       G2 = list(V = diag(3), nu = 0.002),
       G3 = list(V = diag(3), nu = 0.002),
       G4 = list(V = diag(3), nu = 0.002)))
-vcv_full_tree <- ape::vcv.phylo(demo_tree)
 
-demo_data <- cbind(demo_data, "clade_sp" = demo_data$animal)
-
-
-brms_run <- system.time(
-  brms_model <- brm(
-    mvbind(PC1, PC2, PC3) ~ clade-1 + (1|gr(clade_sp, cov = vcv_full_tree, by = clade)) + (1|gr(animal, cov = vcv_full_tree)), 
-    data = demo_data, 
-    family = gaussian(), 
-    data2 = list(vcv_full_tree = vcv_full_tree),
-    chains = 1,
-    iter = 10000,
-    thin = 100
-  )
-)
-
-
-mcmcglmm_run <- system.time(
-  mcmcglmm_model <- MCMCglmm(cbind(PC1, PC2, PC3) ~ trait:clade-1, family = rep("gaussian", ntraits),
-                       random =
-                         ~ us(at.level(clade,1):trait):animal
-                         + us(at.level(clade,2):trait):animal
-                         + us(at.level(clade,3):trait):animal
-                         + us(trait):animal,
-                      rcov=
-                         ~ us(trait):units,
-                       prior= priorRG5, pedigree = demo_tree, data = demo_data, nitt = 10000, burnin = 5000, thin = 100)
-)
-
-
+## The MCMCglmm model
+mcmcglmm_model <- MCMCglmm(cbind(PC1, PC2, PC3) ~ trait:clade-1,
+                     family = rep("gaussian", 3),
+                     random =
+                       ~ us(at.level(clade,1):trait):animal
+                       + us(at.level(clade,2):trait):animal
+                       + us(at.level(clade,3):trait):animal
+                       + us(trait):animal,
+                     rcov = ~ us(trait):units,
+                     prior = priorRG5, pedigree = demo_tree, data = demo_data, nitt = 1000, burnin = 0, thin = 1)
