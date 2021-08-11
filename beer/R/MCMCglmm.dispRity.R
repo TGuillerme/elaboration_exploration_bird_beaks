ef5dad6a79a83d2a71bb426c8cd8d6201e912ec6
TGuillerme <guillert@tcd.ie>
2021-08-11 15:52:31 +0100

    MCMCglmm.dispRity infrastructure ready (but untested)

--- beer/R/MCMCglmm.dispRity.R
+++ beer/R/MCMCglmm.dispRity.R
@@ -4,9 +4,17 @@
 #'
 #' @param data The data used for the MCMCglmm model
 #' @param posteriors A MCMCglmm object, the posteriors of the model
-#' @param group Optional, which group to include from the posteriors (if left empty the residuals and random terms are used)
-#' @param tree Optional, the tree(s) used in the MCMCglmm analyses
-#' @param rename.groups optional, a vector of group names for renaming them.
+#' @param group Optional, a named vector of which group to include from the posteriors (if left empty the random and residual terms are used). See details.
+#' @param tree Optional, the tree(s) used in the MCMCglmm analyses.
+#' @param rename.groups optional, a vector of group names for renaming them. See details.
+#' 
+#' @details
+#' \itemize{
+#'      \item For the \code{group} option, the group names must be ones found in the \code{posteriors} formula in the format \emph{<Type = Term:FactorLevel>} as returned by \code{MCMCglmm.levels(posteriors)}. For example, for returning two random effect, the phylogenetic one (\code{"animal"}) and one for a specific clade (say the 2nd clade) as well as two residual terms for a specific factor (say level 1 and 4) you can use \code{group = c(random = "animal", random = "animal:clade2", residual = "units:myfactor1", residual = "units:myfactor4")}.
+#'      \item For the \code{rename.groups} option, the vector must be of class \code{"character"} and must of the same length as the number of random and residual terms in \code{posteriors} or of \code{group} argument (if used). If the \code{group} argument is left empty, the groups are extracted from the \code{posteriors} in the following order: the random terms first then the residual terms as specified in the \code{posteriors} object formulas (respectively \code{posteriors$Random$formula} and \code{posteriors$Residual$formula}).
+#' 
+#' 
+#' }
 #' 
 #' @examples
 #'
@@ -34,111 +42,94 @@ MCMCglmm.dispRity <- function(data, posteriors, group, tree, rename.groups) {
         ## Is there any classification column?
         classifier <- col_classes[!numerics] %in% "factor"
         if(any(classifier)) {
-            recycled_group_classifier <- which(!numerics)[which(classifier)]
+            group_classifier <- data[,which(!numerics)[which(classifier)], drop = FALSE]
         }
     }
     
     ## Checking the posteriors
     #dispRity_export in: check.class(posteriors, "MCMCglmm")
+    ## Check which dimensions where used
+    dimensions <- match(MCMCglmm.traits(posteriors), colnames(cleaned_data))
+
     ## Extracting the residuals and randoms
+    posterior_levels <- MCMCglmm.levels(posteriors)
+    posterior_terms <- lapply(posterior_levels, split.term.name)
 
-    # terms
-
-    # ## Getting the groups
-    # ## Extracting the group from the posteriors
-    # if(missing(group)) {
-    #     ## Automatically detect the groups
-    # }
-
-    # ## Renaming the groups
-    # rename.groups
-
-    # ## Adding the tree
-
-
-    return(NULL)
-}
-
-## Internal: get the possible levels from a MCMCglmm
-MCMCglmm.levels <- function(MCMCglmm, convert = TRUE) {
-    convert.term.name <- function(one_term) {
-        ## Return the term is simple, keep it like that
-        if(length(grep(":", one_term)) == 0) {
-            return(one_term)
-        } else {
-            ## Split the term 
-            elements <- strsplit(one_term, ":")[[1]]
-            ## Remove the trait component
-            if(any(grep("trait", elements))) {
-                elements <- elements[-grep("trait", elements)]
-            }
-
-            if(length(elements) == 1) {
-                ## That's the element name
-                return(gsub(" ", "", elements))
-            } else {
-                ## Remove spaces and get the most nested element
-                return(paste(rev(unname(sapply(elements, function(X) gsub(" ", "", gsub("[^[:alnum:] ]", "", rev(strsplit(X, "\\(")[[1]])[1]))))), collapse = ":"))
-            }
-
-        }
-    }
-
-    ## Get the random terms
-    random_formula <- as.character(MCMCglmm$Random$formula[2])
-    if(length(random_formula) == 0) {
-        random_terms <- NULL
-    } else {
-        random_terms <- strsplit(random_formula, "\\+")[[1]]
-    }
+    ## Extracting the formula
+    formula <- list(Fixed     = posteriors$Fixed$formula,
+                    Random    = posteriors$Random$formula,
+                    Residuals = posteriors$Residual$formula)
     
-    ## Get the residuals terms
-    residuals_formula <- as.character(MCMCglmm$Residual$formula[2])
-    if(length(residuals_formula) == 0) {
-        residual_terms <- NULL
+    ## Extracting the group from the posteriors
+    extracted_group <- lapply(posterior_terms, get.one.group, group_classifier, elements)
+    names(extracted_group) <- posterior_levels
+
+    ## Setting the groups
+    if(missing(group)) {
+        subsets <- extracted_group
     } else {
-        residual_terms <- strsplit(residuals_formula, "\\+")[[1]]
-    }
-
-    ## No terms!
-    if(is.null(random_terms) && is.null(residual_terms)) {
-        return(c("residual" = "units"))
-    }
-
-    ## Convert the names to make them look fancy
-    if(convert) {
-        if(!is.null(random_terms)) {
-            random_terms <- unname(sapply(random_terms, convert.term.name))
-        }
-        if(!is.null(residual_terms)) {
-            residual_terms <- unname(sapply(residual_terms, convert.term.name))
+        if(is.null(names(group))) {
+            #dispRity_export in: MAKE dispRity STOP STYLE
+            stop("group must be a named character vector.")#dispRity_export out:
+        } else {
+            if(!all(selected_groups <- posterior_levels %in% group)) {
+                ## Select only the specific groups
+                subsets <- extracted_group[selected_groups]
+            } else {
+                #dispRity_export in: MAKE dispRity STOP STYLE
+                stop("groups not found in posteriors. Check MCMCglmm(posteriors) for the group names.")#dispRity_export out:                
+            }
         }
     }
 
-    ## Get the terms names
-    all_terms <- c(random_terms, residual_terms)
-    names(all_terms) <- c(rep("random", length(random_terms)), rep("residual", length(residual_terms)))
+    ## Getting the covar matrices per group
+    covar_matrices <- MCMCglmm.traits(posteriors)
 
-    return(all_terms)
+    ## Renaming the groups
+    if(!missing(rename.groups)) {
+        if(length(rename.groups) != length(subsets)) {
+            #dispRity_export in: MAKE dispRity STOP STYLE
+            stop("rename.groups must the same length as group.")#dispRity_export out:
+        }
+        names(subsets) <- rename.groups
+    }
+
+    ## Adding the tree
+    if(!missing(tree)) {
+        ## Create a dispRity style object with tree
+        stop("do tree")
+    } else {
+        ## Create a dispRity style object without tree
+        return(make.dispRity(data = cleaned_data, call = list("subsets" = "MCMCglmm"), subsets = subsets, tree = tree))
+    }
 }
 
-## Internal: get the number of traits from a MCMCglmm
-MCMCglmm.traits <- function(MCMCglmm) {
-
-    ## Get the variables
-    variables <- as.character(MCMCglmm$Fixed$formula[2])
-
-    ## Are the variables bunched in an expression (e.g. "c()")?
-    if(any(grep("\\(", variables))) {
-        ## Remove the brackets of the expression
-        variables <- strsplit(strsplit(variables, "\\(")[[1]][2], "\\)")[[1]]
+## create the group list
+get.one.group <- function(one_term, group_classifier, elements) {
+    ## Animal term (phylogeny)
+    if(one_term$term == "animal" && is.null(one_term$factor) && is.null(one_term$level)) {
+        ## The group is the full phylogeny (dispRity format)
+        #return(1:length(elements))
+        return(list(elements = matrix(1:length(elements), ncol = 1)))
+    } else {
+        ## Get the factor in group_classifier (dispRity format)
+        #return(which(group_classifier[,one_term$factor] == levels(group_classifier[,one_term$factor])[one_term$level]))
+        return(list(elements = matrix(which(group_classifier[,one_term$factor] == levels(group_classifier[,one_term$factor])[one_term$level]), ncol = 1)))
     }
+}
 
-    ## Are there multiple variables in that expression (e.g. ",")?
-    if(any(grep(",", variables))) {
-        ## Remove the commas and spaces
-        variables <- gsub(" ", "", strsplit(variables, ",")[[1]])
+## Splitting a term name
+split.term.name <- function(one_term) {
+    ## Initialise the factor and level
+    factor <- level <- NULL
+    ## Split the term
+    split_term <- strsplit(one_term, ":")[[1]]
+    ## Get the second part of the term
+    if(length(split_term) > 1) {
+        level  <- as.numeric(gsub(".*?([[:digit:]]+)", "\\1", split_term[[2]]))
+        factor <- as.character(gsub(level, "", split_term[[2]]))
     }
-
-    return(variables)
+    return(list(term   = split_term[[1]],
+                factor = factor,
+                level  = level))
 }
