76e8419f2e2c926d9f3cc6a973aaaa6292e01e97
TGuillerme <guillert@tcd.ie>
2021-08-16 17:11:40 +0100

    Create test-sauron.plot.R

---
+++ beer/tests/testthat/test-sauron.plot.R
@@ -0,0 +1,14 @@
+## Test
+test_that("sauron.plot works", {
+
+    # ## Sanitizing
+    # expect_error(sauron.plot())
+
+    # ## Right output
+    # expect_is(
+    #     sauron.plot()
+    #     , "class")
+    # expect_equal(
+    #     dim(sauron.plot())
+    #     , dim)
+})
