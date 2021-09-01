0d09e9fd08403bb9dd4b33291dcf3ac622a26e38
TGuillerme <guillert@tcd.ie>
2021-09-01 16:58:17 +0100

    added mean smaplign option

--- beer/R/covar.utilities.R
+++ beer/R/covar.utilities.R
@@ -8,7 +8,7 @@
 #' @usage axis.covar(data, n, level = 0.95, axis = 1)
 #'
 #' @param data a \code{dispRity} object with a \code{covar} element.
-#' @param sample optional, one or more specific posterior sample IDs (is ignored if n is used).
+#' @param sample optional, one or more specific posterior sample IDs (is ignored if n is used) or a function to summarise all axes.
 #' @param n optional, a random number of covariance matrices to sample (if left empty, all are used).
 #' @param level which confidence interval level to use (default is \code{0.95}).
 #' @param axis which major axis to calculate (default is \code{1}, the first one).
@@ -45,16 +45,28 @@ get.covar <- function(data, sample, n) {
 
 axis.covar <- function(data, sample, n, dimensions, level = 0.95, axis = 1) {
     
-    ## Get the covar matrices
-    selected_covars <- get.covar(data, sample, n)
-    
     ## Sanitizing on level and axis
 
     ## Selecting the dimensions (all by default)
     if(missing(dimensions)) {
         dimensions <- data$call$dimensions
     }
+
+    ## Handle sample
+    if(!missing(sample) && (is(sample, "function") || is(sample, "standardGeneric"))) {
+        return(lapply(
+                ## Get the axes from the selected covars
+                    lapply(
+                    ## Get the covar matrices
+                    get.covar(data, n), lapply, get.one.axis, axis = axis, level = level, dimensions = dimensions),
+                ## Get the mean of all the axes
+                function(one_group, fun) list(apply(simplify2array(one_group), 1:2, fun)), fun = sample))
+    }
+
+    ## Get the covar matrices
+    selected_covars <- get.covar(data, sample, n)
+
     ## Select all the axis
-    return(lapply(selected_covars, lapply, get.one.axis, axis = 1, level = level, dimensions = dimensions))
+    return(lapply(selected_covars, lapply, get.one.axis, axis = axis, level = level, dimensions = dimensions))
 }
 
