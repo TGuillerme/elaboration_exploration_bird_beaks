b4627b5cb7e5248e4ff4c861e8d29ca0fb112957
TGuillerme <guillert@tcd.ie>
2021-09-07 16:20:11 +0100

    covar.projections.wrapper devel paused

--- beer/R/covar.projections.wrapper.R
+++ beer/R/covar.projections.wrapper.R
@@ -44,60 +44,60 @@ covar.projections.wrapper <- function(data, type, base, average, major.axis = 1,
 
     ## Check method measure = c("position", "distance", "degree")
 
-    ## 1 - get major axis
-    if(!missing(average)) {
-        major_axes <- axis.covar(data, sample = average, axis = major.axis, level = level)
-    } else {
-        major_axes <- axis.covar(data, axis = major.axis, level = level)
-    }
+    # ## 1 - get major axis
+    # if(!missing(average)) {
+    #     major_axes <- axis.covar(data, sample = average, axis = major.axis, level = level)
+    # } else {
+    #     major_axes <- axis.covar(data, axis = major.axis, level = level)
+    # }
 
-    ## A - Type between:
-    if(type == "between") {
-        wrap.dispRity.between <- function(measure, data, list_of_pairs) {
-            return(dispRity.covar(data, metric = projections, between.groups = list_of_pairs, measure = measure, dimensions = data$call$dimensions)$disparity)
-        }
+    # ## A - Type between:
+    # if(type == "between") {
+    #     wrap.dispRity.between <- function(measure, data, list_of_pairs) {
+    #         return(dispRity.covar(data, metric = projections, between.groups = list_of_pairs, measure = measure, dimensions = data$call$dimensions)$disparity)
+    #     }
 
-        ## 2 - get the groups
-        if(!missing(base)) {
-            list_of_pairs <- unlist(apply(combn(1:n.subsets(data), 2), 2, list), recursive = FALSE)
-        } else {
-            base_id <- which(names(size.subsets(data)) == base)
-            list_of_pairs <- lapply(as.list(1:(n.subsets(data)-1)), function(x,y) c(x, y), y = base_id)
-        }
+    #     ## 2 - get the groups
+    #     if(!missing(base)) {
+    #         list_of_pairs <- unlist(apply(combn(1:n.subsets(data), 2), 2, list), recursive = FALSE)
+    #     } else {
+    #         base_id <- which(names(size.subsets(data)) == base)
+    #         list_of_pairs <- lapply(as.list(1:(n.subsets(data)-1)), function(x,y) c(x, y), y = base_id)
+    #     }
 
-        ## Get all results
-        results <- lapply(as.list(measure), wrap.dispRity.between, data, list_of_pairs)
-    }
+    #     ## Get all results
+    #     results <- lapply(as.list(measure), wrap.dispRity.between, data, list_of_pairs)
+    # }
 
-    ## B - Type within
-    if(type == "within") {
-        ## Wrapper for within function
-        wrap.dispRity.within <- function(measure, data, axes) {
-            ## Calculate the measure
-            results <- lapply(axes, function(axes, data) dispRity(data, dimensions = data$call$dimensions, metric = projections, point1 = axes[1, ], point2 = axes[2, ])$disparity, data = data)
-            ## Combine all the results together
-            results <- lapply(1:length(results[[1]]), function(u) do.call(cbind, lapply(results, `[[`, u)))
-            return(lapply(results, function(res) do.call(cbind, res)))
-        }
+    # ## B - Type within
+    # if(type == "within") {
+    #     ## Wrapper for within function
+    #     wrap.dispRity.within <- function(measure, data, axes) {
+    #         ## Calculate the measure
+    #         results <- lapply(axes, function(axes, data) dispRity(data, dimensions = data$call$dimensions, metric = projections, point1 = axes[1, ], point2 = axes[2, ])$disparity, data = data)
+    #         ## Combine all the results together
+    #         results <- lapply(1:length(results[[1]]), function(u) do.call(cbind, lapply(results, `[[`, u)))
+    #         return(lapply(results, function(res) do.call(cbind, res)))
+    #     }
 
-        ## If base
-        if(!missing(base)) {
-            ## Select the non-base subsets
-            base_id <- which(names(size.subsets(data)) == base)
-            non_base_id <-  which(names(size.subsets(data)) != base)
+    #     ## If base
+    #     if(!missing(base)) {
+    #         ## Select the non-base subsets
+    #         base_id <- which(names(size.subsets(data)) == base)
+    #         non_base_id <-  which(names(size.subsets(data)) != base)
 
-            ## Run the measurements for all the groups
-            results <- lapply(measure, wrap.dispRity.within, data = get.subsets(data, non_base_id), axes = major_axes[[base_id]])
-        } else {
-            ## Run the measurements for each group
-            results <- list()
-            for(one_subset in 1:n.subsets(data)) {
-                results[[one_subset]] <- lapply(measure, wrap.dispRity.within, data = get.subsets(data, one_subset), axes = major_axes[[one_subset]])
-            }
-        }
+    #         ## Run the measurements for all the groups
+    #         results <- lapply(measure, wrap.dispRity.within, data = get.subsets(data, non_base_id), axes = major_axes[[base_id]])
+    #     } else {
+    #         ## Run the measurements for each group
+    #         results <- list()
+    #         for(one_subset in 1:n.subsets(data)) {
+    #             results[[one_subset]] <- lapply(measure, wrap.dispRity.within, data = get.subsets(data, one_subset), axes = major_axes[[one_subset]])
+    #         }
+    #     }
 
-        ## Standardise the results
-    }
+    #     ## Standardise the results
+    # }
 
     ## Standardise the results
 
