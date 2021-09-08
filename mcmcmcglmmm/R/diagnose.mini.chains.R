70ed5c983c31009928a970414cb9c1d688eac1b2
TGuillerme <guillert@tcd.ie>
2021-09-08 13:20:48 +0100

    Isolated mcmcmcglmmm parts

---
+++ mcmcmcglmmm/R/diagnose.mini.chains.R
@@ -0,0 +1,16 @@
+#' @title diagnose.mini.chains
+#'
+#' @description Diagnose a set of mini chains results
+#'
+#' @param mini.chains a MCMCglmm result
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+diagnose.mini.chains <- function(mini.chains) {
+    return("prototype")
+}
