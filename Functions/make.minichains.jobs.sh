940a401013a80e3fc821d1d12b10e6472565aabc
TGuillerme <guillert@tcd.ie>
2021-09-08 16:14:27 +0100

    testing make.minichains

--- Functions/make.minichains.jobs.sh
+++ Functions/make.minichains.jobs.sh
@@ -1,28 +1,66 @@
-## Shell part
-
 #!/bin/bash
-#
-#$ -l h_rt=00:01:00
-# below -m option can have any combination of b , e or a  to imply when to to send email where;
-#    b = begining of job  e = end of job  a = in case job gets aborted unexpectedly 
-#$ -m ae
-#$ -M guillert@tcd.ie
+##########################
+# Script for initialising the minichains
+##########################
+#SYNTAX: sh make.minichains.jobs.sh -chain <chain_name> -n <replicates> -p <path (optional)>
+#with:
+#-chain <chain_name> the name of the chain
+#-n <number_of_replicates> the number of replicates
+#-p <path> optional, the path to the chain
+##########################
+#guillert(at)tcd.ie - 2021/09/08
+##########################
 
-## Load R
-module load apps/R/4.0.3/gcc-8.2.0
+#INPUT
+## Input values
+while [[ $# -gt 1 ]]
+do
+key="$1"
 
-## Run the script
-Rscript ${CHAINNAME}_${REPLICATE}.script.R
+case $key in
+    -c|--chain)
+        CHAINNAME="$2"
+        shift
+        ;;
+    -b|--replicates)
+        REPLICATES="$2"
+        shift
+        ;;        
+    -p|--path)
+        PATH="$2"
+        ;;
+        *)
+        ;;
+esac
+shift
+done
 
+## Creating the batch job template
+cp job_template.job ${PATH}${CHAINNAME}_template.job
+echo "" >> ${PATH}${CHAINNAME}_template.job
+echo "## Run the script" >> ${PATH}${CHAINNAME}_template.job
+echo "Rscript ${PATH}${CHAINNAME}_<REPLICATE>.script.R" >> ${PATH}${CHAINNAME}_template.job
 
+## Creating the Rscript template
+echo ".libPaths(\"~/homelib\")" > ${PATH}${CHAINNAME}_template.R
+echo "library(MCMCglmm)" >> ${PATH}${CHAINNAME}_template.R
+echo "library(mcmcmcglmmm)" >> ${PATH}${CHAINNAME}_template.R
+echo "load(\"${PATH}${CHAINNAME}.rda\")" >> ${PATH}${CHAINNAME}_template.R
+echo "model <- run.mini.chains(${CHAINNAME}, 1)" >> ${PATH}${CHAINNAME}_template.R
+echo "save(model, file = \"${PATH}${CHAINNAME}_<REPLICATE>.rda\")" >> ${PATH}${CHAINNAME}_template.R
 
+## Looping through each templates to create the replicates
+for (( i = 1; i <= ${REPLICATES}; i++ ));
+do
+    sed 's/<REPLICATE>/'"${i}"'/g' ${PATH}${CHAINNAME}_template.job > ${PATH}${CHAINNAME}_${i}.job
+    sed 's/<REPLICATE>/'"${i}"'/g' ${PATH}${CHAINNAME}_template.R > ${PATH}${CHAINNAME}_${i}.R
+done
 
+## Cleaning
+rm tmp
+rm ${PATH}${CHAINNAME}_template.R
+rm ${PATH}${CHAINNAME}_template.job
 
-
-
-## R part
-library(MCMCglmm)
-library(beer)
-load(${CHAINNAME})
-model <- run.mini.chains(${CHAINNAME}, 1)
-save(model, file = "${CHAINNAME}_${REPLICATE}.rda")
+## Prompt
+echo "## To qsub all chains, run the following:"
+echo "for file in ${PATH}${CHAINNAME}_*.job ; do echo ${file} ; qsub ${file}; done"
