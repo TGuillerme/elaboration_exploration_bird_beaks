a8491d56a4395a6c1d9eb04e13ede151de934782
TGuillerme <guillert@tcd.ie>
2021-09-13 16:47:54 +0100

    Draft for speeding up the functions

---
+++ beer/R/covar.projections.wrapper_fun.R
@@ -0,0 +1,10 @@
+## Between type
+# dispRity.covar(data, metric = projections.covar, between.groups = list_of_pairs, measure = measure)$disparity
+
+
+
+## Run projections faster!
+
+
+## Within type
+# dispRity(data, dimensions = data$call$dimensions, metric = projections, point1 = axes[1, ], point2 = axes[2, ])$disparity
