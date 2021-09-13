7b53233c935c6d5fb31419b2c947eeaadb6cfb2f
TGuillerme <guillert@tcd.ie>
2021-09-13 16:32:59 +0100

    Missing verbose

--- beer/R/covar.projections.wrapper.R
+++ beer/R/covar.projections.wrapper.R
@@ -4,11 +4,13 @@
 #'
 #' @param data a \code{dispRity} object containing a covar component
 #' @param type either \code{"within"} for the projection of the group onto itself or \code{"between"} for the projections of the groups onto the \code{base} (see details)
+#' @param sample optional, one or more specific posterior sample IDs (is ignored if n is used) or a function to summarise all axes.
+#' @param n optional, a random number of covariance matrices to sample (if left empty, all are used).
 #' @param base optional, if \code{type = "between"}, a \code{character} string for the name of the subset in data to use as a baseline (e.g. the phylogeny) or if \code{type = "within"} the major axis to use as a base (see details).
-#' @param average optional, if \code{type = "within"}, a the function for averaging the major axes (see details)
 #' @param major.axis which major axis to use (default is \code{1})
 #' @param level the confidence interval to estimate the major axis (default is \code{0.95})
 #' @param measure which parameters to measure from the projection. Can be any of the following \code{"c(position, distance, degree)"} (default) for respectively the distance on the projection, the distance from the projection and the angle of the projection.
+#' @param verbose Whether to be verbose (\code{TRUE}) or not (\code{FALSE}, default).
 #' 
 #' @details
 #' Effectively, the wrapper runs either of the following function (simplified here):
@@ -32,7 +34,7 @@
 #' @author Thomas Guillerme
 #' @export
 
-covar.projections.wrapper <- function(data, type, base, average, major.axis = 1, level = 0.95, measure = c("position", "distance", "degree")) {
+covar.projections.wrapper <- function(data, type, sample, n, base, average, major.axis = 1, level = 0.95, measure = c("position", "distance", "degree"), verbose = FALSE) {
 
     ## Check class data (dispRity)
  
@@ -44,63 +46,95 @@ covar.projections.wrapper <- function(data, type, base, average, major.axis = 1,
 
     ## Check method measure = c("position", "distance", "degree")
 
-    # ## 1 - get major axis
-    # if(!missing(average)) {
-    #     major_axes <- axis.covar(data, sample = average, axis = major.axis, level = level)
-    # } else {
-    #     major_axes <- axis.covar(data, axis = major.axis, level = level)
-    # }
+    ## 1 - get major axis
+    if(verbose) message("calculating the major axis:...")
+    major_axes <- axis.covar(data, sample = sample, n = n, axis = major.axis, level = level)
+    # major_axes <- axis.covar(data, n = n)
+    if(verbose) message("Done.")
 
-    # ## A - Type between:
-    # if(type == "between") {
-    #     wrap.dispRity.between <- function(measure, data, list_of_pairs) {
-    #         return(dispRity.covar(data, metric = projections, between.groups = list_of_pairs, measure = measure, dimensions = data$call$dimensions)$disparity)
-    #     }
+    ## 2 - get the data
+    if(!missing(n) || !missing(sample)) {
+        data$MCMCglmm$covars <- get.covar(data, n = n, sample = sample)
+    }
 
-    #     ## 2 - get the groups
-    #     if(!missing(base)) {
-    #         list_of_pairs <- unlist(apply(combn(1:n.subsets(data), 2), 2, list), recursive = FALSE)
-    #     } else {
-    #         base_id <- which(names(size.subsets(data)) == base)
-    #         list_of_pairs <- lapply(as.list(1:(n.subsets(data)-1)), function(x,y) c(x, y), y = base_id)
-    #     }
+    ## A - Type between:
+    if(type == "between") {
+        wrap.dispRity.between <- function(measure, data, list_of_pairs, verbose) {
 
-    #     ## Get all results
-    #     results <- lapply(as.list(measure), wrap.dispRity.between, data, list_of_pairs)
-    # }
+            if(verbose) {
+                message("PLACEHOLDER:...")
 
-    # ## B - Type within
-    # if(type == "within") {
-    #     ## Wrapper for within function
-    #     wrap.dispRity.within <- function(measure, data, axes) {
-    #         ## Calculate the measure
-    #         results <- lapply(axes, function(axes, data) dispRity(data, dimensions = data$call$dimensions, metric = projections, point1 = axes[1, ], point2 = axes[2, ])$disparity, data = data)
-    #         ## Combine all the results together
-    #         results <- lapply(1:length(results[[1]]), function(u) do.call(cbind, lapply(results, `[[`, u)))
-    #         return(lapply(results, function(res) do.call(cbind, res)))
-    #     }
+                results <- dispRity.covar(data, metric = projections.covar, between.groups = list_of_pairs, measure = measure)$disparity
 
-    #     ## If base
-    #     if(!missing(base)) {
-    #         ## Select the non-base subsets
-    #         base_id <- which(names(size.subsets(data)) == base)
-    #         non_base_id <-  which(names(size.subsets(data)) != base)
+                message("Done.")
+                return(results)
+            } 
 
-    #         ## Run the measurements for all the groups
-    #         results <- lapply(measure, wrap.dispRity.within, data = get.subsets(data, non_base_id), axes = major_axes[[base_id]])
-    #     } else {
-    #         ## Run the measurements for each group
-    #         results <- list()
-    #         for(one_subset in 1:n.subsets(data)) {
-    #             results[[one_subset]] <- lapply(measure, wrap.dispRity.within, data = get.subsets(data, one_subset), axes = major_axes[[one_subset]])
-    #         }
-    #     }
+            ## Use normal dispRity here
+            # dispRity.covar(data, metric = projections.covar, between.groups = list_of_pairs, measure = measure, dimensions = data$call$dimensions, point1 = axes[1, ], point2 = axes[2, ])$disparity
+            return(dispRity.covar(data, metric = projections.covar, between.groups = list_of_pairs, measure = measure)$disparity)
+        }
 
-    #     ## Standardise the results
-    # }
+        ## 2 - get the groups
+        if(missing(base)) {
+            list_of_pairs <- unlist(apply(combn(1:n.subsets(data), 2), 2, list), recursive = FALSE)
+        } else {
+            base_id <- which(names(size.subsets(data)) == base)
+            list_of_pairs <- lapply(as.list(1:(n.subsets(data)-1)), function(x,y) c(x, y), y = base_id)
+        }
 
-    ## Standardise the results
+        ## Get all results
+        results <- lapply(as.list(measure), wrap.dispRity.between, data, list_of_pairs, verbose)
 
+        ## Remove the elements part
+        results <- lapply(results, lapply, function(x) {x$elements <- NULL; return(matrix(unlist(x), nrow = 1))})
+        names(results) <- measure
+    }
 
-    return()
+    ## B - Type within
+    if(type == "within") {
+        ## Wrapper for within function
+        wrap.dispRity.within <- function(measure, data, axes, verbose) {
+            ## Calculate the measure
+            if(verbose) message(".")
+            results <- lapply(axes, function(axes, data) dispRity(data, dimensions = data$call$dimensions, metric = projections, point1 = axes[1, ], point2 = axes[2, ])$disparity, data = data)
+            ## Combine all the results together
+            results <- lapply(1:length(results[[1]]), function(u) do.call(cbind, lapply(results, `[[`, u)))
+            return(lapply(results, function(res) do.call(cbind, res)))
+        }
+
+        ## If base
+        if(!missing(base)) {
+            ## Select the non-base subsets
+            base_id <- which(names(size.subsets(data)) == base)
+            non_base_id <-  which(names(size.subsets(data)) != base)
+
+            ## Run the measurements for all the groups on the base
+            if(verbose) message("PLACEHOLDER:")
+            results <- lapply(measure, wrap.dispRity.within, data = get.subsets(data, non_base_id), axes = major_axes[[base_id]], verbose)
+            if(verbose) message("Done.")
+        
+        } else {
+        
+            ## Run the measurements for each group on itself
+            if(verbose) message("PLACEHOLDER:")
+            non_base_id <- 1:n.subsets(data)
+            results <- list()
+            for(one_subset in 1:n.subsets(data)) {
+                if(verbose) message("...")
+                results[[one_subset]] <- lapply(measure, wrap.dispRity.within, data = get.subsets(data, one_subset), axes = major_axes[[one_subset]], verbose = FALSE)
+            }
+            if(verbose) message("Done.")
+
+            ## Standardise the results
+            reorder.results <- function(one_measure, results) {return(lapply(results, function(x, one_measure) return(x[[one_measure]][[1]]),    one_measure = 1))}
+            results <- lapply(as.list(1:length(measure)), reorder.results, results = results)
+        }
+
+        ## Standardising the output
+        names(results) <- measure
+        results <- lapply(results, function(x, names) {names(x) <- names; return(x)}, names = names(size.subsets(data))[non_base_id])
+    }
+
+    return(results)
 }
---
+++ beer/tests/testthat/test-covar.projections.wrapper.R
@@ -0,0 +1,74 @@
+## Test
+test_that("covar.projections.wrapper works", {
+    ## Loading the MCMCglmm
+    load("../../../Data/processed/combined_results.rda")
+    ## Loading the tree and data
+    data(demo_data)
+    data(demo_tree)
+
+    data <- MCMCglmm.dispRity(
+                      data          = demo_data,
+                      posteriors    = combined_results,
+                      group         = MCMCglmm.levels(combined_results)[1:4],
+                      rename.groups = c("gulls", "plovers", "sandpipers", "phylogeny"))
+    
+    ## Test between no base
+    test <- covar.projections.wrapper(data, type = "between", n = 7, verbose = FALSE)
+    expect_equal(names(test), c("position", "distance", "degree"))
+    expect_equal(names(test[[1]]), c("gulls:plovers", "gulls:sandpipers",  "gulls:phylogeny", "plovers:sandpipers",  "plovers:phylogeny", "sandpipers:phylogeny"))
+    expect_equal(dim(test[[1]][[1]]), c(1,7))
+    
+    ## Test between average phylo base
+    test <- covar.projections.wrapper(data, type = "between", sample = mean, base = "phylogeny")
+    expect_equal(names(test), c("position", "distance", "degree"))
+    expect_equal(names(test[[1]]), c("gulls:phylogeny", "plovers:phylogeny", "sandpipers:phylogeny"))
+    expect_equal(dim(test[[1]][[1]]), c(1,1))
+
+    ## Test between all phylo bases
+    test <- covar.projections.wrapper(data, type = "between", sample = c(1,2,3,4,5), base = "phylogeny")
+    expect_equal(names(test), c("position", "distance", "degree"))
+    expect_equal(names(test[[1]]), c("gulls:phylogeny", "plovers:phylogeny", "sandpipers:phylogeny"))
+    expect_equal(dim(test[[1]][[1]]), c(1,5))
+
+    ## Test within no base
+    test <- covar.projections.wrapper(data, type = "within", n = 5, measure = c("degree", "distance"))
+    expect_equal(names(test), c("degree", "distance"))
+    expect_equal(names(test[[1]]), c("gulls", "plovers", "sandpipers", "phylogeny"))
+    for(i in 1:length(test[[1]])) {
+        expect_equal(dim(test[[1]][[i]]), c(unname(size.subsets(data)[i]),5))
+        expect_equal(dim(test[[2]][[i]]), c(unname(size.subsets(data)[i]),5))
+    }
+
+    ## Test within average phylo base
+    test <- covar.projections.wrapper(data, type = "within", sample = mean, measure = c("position"), base = "phylogeny")
+    expect_equal(names(test), c("position"))
+    expect_equal(names(test[[1]]), c("gulls", "plovers", "sandpipers"))
+    for(i in 1:length(test[[1]])) {
+        expect_equal(dim(test[[1]][[i]]), c(unname(size.subsets(data)[i]),1))
+    }
+
+    ## Test within all phylo bases
+    test <- covar.projections.wrapper(data, type = "within", measure = c("position"), base = "phylogeny")
+    expect_equal(names(test), c("position"))
+    expect_equal(names(test[[1]]), c("gulls", "plovers", "sandpipers"))
+    for(i in 1:length(test[[1]])) {
+        expect_equal(dim(test[[1]][[i]]), c(unname(size.subsets(data)[i]),1000))
+    }
+})
+
+test_that("covar.projections.wrapper verbose works", {
+    # ## Loading the MCMCglmm
+    # load("../../../Data/processed/combined_results.rda")
+    # ## Loading the tree and data
+    # data(demo_data)
+    # data(demo_tree)
+
+    # data <- MCMCglmm.dispRity(
+    #                   data          = demo_data,
+    #                   posteriors    = combined_results,
+    #                   group         = MCMCglmm.levels(combined_results)[1:4],
+    #                   rename.groups = c("gulls", "plovers", "sandpipers", "phylogeny"))
+    
+    # ## Test between no base
+    # verbose <- capture_messages(test <- covar.projections.wrapper(data, type = "between", n = 30, verbose = TRUE))
+})
