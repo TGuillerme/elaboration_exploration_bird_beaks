158a00513cf33b4320b18955719966687eaa4e78
TGuillerme <guillert@tcd.ie>
2021-09-27 18:10:33 +0100

    added phytools version

--- Functions/circular.plot.R
+++ Functions/circular.plot.R
@@ -16,14 +16,14 @@
 #BiocManager::install("ggtreeExtra")
 #BiocManager::install("phyloseq")
 
-library(ggplot2)
-library(ggtreeExtra)
-library(ggtree)
-library(phyloseq)
-library(dplyr)
+# library(ggplot2)
+# library(ggtreeExtra)
+# library(ggtree)
+# library(phyloseq)
+# library(dplyr)
 
-tree <- demo_tree
-data <- results_phylo$position
+# tree <- demo_tree
+# data <- results_phylo$position
 
 circular.plot <- function(data, tree, col.tips, vals.gradient, vals.cent.tend = median, tree.args = list(layout = "fan", open.angle = 10), data.args, data.plot.fun = ggplot2::geom_boxplot, ...) {
 
@@ -143,52 +143,122 @@ circular.plot <- function(data, tree, col.tips, vals.gradient, vals.cent.tend = 
                 )        
     }
     return(tree_plot)
-
-
-p3 <- fruit_plot(
-           p = tree_plot, 
-           data=data_frame, 
-           geom=data.plot.fun,
-           mapping=aes(y=tips, x=values, fill=central),
-           size=2.5, 
-           starstroke=0
-      )
-
-
-    test <- tree_plot + 
-            geom_fruit(
-                 data    = data_frame,
-                 geom    = data.plot.fun,
-                 mapping = aes(
-                             y=tips,
-                             x=values,
-                             group = central
-                           )
-                )    
-
 }
 
-plot <- circular.plot(data = results_phylo$position, tree = demo_tree, col.tips = c("orange", "blue", "darkgreen"), vals.gradient = c("blue", "orange"))
-
-   
 
 
+#@param data dispRity data
+#@param tree the tree
+#@param gradient optional, a colour gradient
+#@param clades optional, a vector of colour for each clade
+#@param clade.names optional, whether to add the clade names or not
+#@param legend logical, whether to add the legend (TRUE) or not (FALSE)
+#@param fun which phytools function to use (default is phytools::plotTree.wBars)
+#@param ... any arguments to be passed to the phytools functions
+
+
+circular.plot.phytools <- function(data, tree, gradient, clades, clade.names = FALSE, legend = FALSE, fun = phytools::plotTree.wBars, ...) {
+
+    dots <- list(...)
+
+    ## Placeholder for the nodes labels
+    clade_nodes <- numeric()
+    if(!missing(clades)) {
+        ## Vector of clade colours
+        if(length(clades) != n.subsets(data)) {
+            clades <- rep(clades, n.subsets(data))
+        }
+    }
+    
+    ## Set up the data
+    list_of_tips <- tree$tip.label
+    data_sorting <- matrix(NA, nrow = length(list_of_tips), ncol = data$call$bootstrap[[1]], dimnames = list(list_of_tips))
+    n_groups <- n.subsets(data)
+    ## Fill the matrix
+    for(one_group in 1:n_groups) {
+        ## Select the rows in the matrix (in the tree)
+        selected_rows <- rownames(data_sorting) %in% rownames(data$disparity[[one_group]]$elements)
+        ## Select the rows in the data (the disparity)
+        selected_tips <- rownames(data$disparity[[one_group]]$elements) %in% rownames(data_sorting)[selected_rows]
+
+        ## Fill the rows in common
+        if(any(selected_rows) && any(selected_tips)) {
+            data_sorting[selected_rows, ] <- data$disparity[[one_group]]$elements[selected_tips, ]
+        }
+        ## Find the MRCAs for the node plottings
+        clade_nodes[one_group] <- getMRCA(tree, tip = rownames(data$disparity[[one_group]]$elements[selected_tips, ]))
+        names(clade_nodes)[one_group] <- names(data$disparity)[one_group]
+    }
+
+    ## Get the central tendencies
+    centrals <- apply(data_sorting, 1, median)
+
+    ## Get the colours for the central values
+    ranged_central <- centrals - min(centrals)
+    ranged_central <- ranged_central/max(ranged_central)
+    data_cols <- rgb(colorRamp(c(gradient[1], gradient[2]))(ranged_central)/255)
+
+
+    ## Get the phytools args
+    plotTree_args <- list()
+    plotTree_args$tree <- tree
+    plotTree_args$x <- centrals
+    plotTree_args$col <- data_cols
+    if(is.null(dots$type)) {
+        plotTree_args$type <- "fan"
+    } else {
+        plotTree_args$type <- dots$type
+    }
+    if(!is.null(dots$border)) {
+        plotTree_args$border <- dots$border
+    }
+    if(!is.null(dots$width)) {
+        plotTree_args$width <- dots$width
+    }
+    if(!is.null(dots$args.plotTree)) {
+        plotTree_args$args.plotTree <- dots$args.plotTree
+    }
+    if(!is.null(dots$args.barplot)) {
+        plotTree_args$args.barplot <- dots$args.barplot
+    }
+
+    ## Plot the tree
+    do.call(fun, plotTree_args)
+    
+    ## And with clade labels for the beauty of it
+    if(clade.names) {
+        ## clade_args
+        clade_args <- list()
+        if(is.null(dots$mark.node)) {
+            clade_args$mark.node <- FALSE
+        }
+        if(!is.null(dots$lab.offset)) {
+            clade_args$lab.offset <- dots$lab.offset
+        }        
+        if(!is.null(dots$ln.offset)) {
+            clade_args$ln.offset <- dots$ln.offset
+        }
+        if(!is.null(dots$lwd)) {
+            clade_args$lwd <- dots$lwd
+        }
+        for(i in 1:length(clade_nodes)) {
+            clade_args$text <- names(clade_nodes)[i]
+            clade_args$node <- clade_nodes[i]
+            clade_args$col <- clades[i]
+            do.call(phytools::arc.cladelabels, clade_args)
+        }
+    }
+
+    if(legend) {
+        ## Adding the legend
+        legend("topright", labels = round(range(centrals), digits = 3), pch = 15, col = c(gradient[1], gradient[2]))
+    }
+
+    return(invisible())
+}
 
 
 
 
 
 
-
-
-
-# p <- p +
-#      scale_fill_discrete(
-#          name="Phyla",
-#          guide=guide_legend(keywidth=0.8, keyheight=0.8, ncol=1)
-#      ) +
-#      theme(
-#          legend.title=element_text(size=9), # The title of legend 
-#          legend.text=element_text(size=7) # The label text of legend, the sizes should be adjust with dpi.
-#      )
-# p
