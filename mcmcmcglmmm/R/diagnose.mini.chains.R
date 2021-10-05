187a2c5b1e40ed332a285f5f222a860431eaf0ee
TGuillerme <guillert@tcd.ie>
2021-10-05 13:25:48 +0100

    draft update

--- mcmcmcglmmm/R/diagnose.mini.chains.R
+++ mcmcmcglmmm/R/diagnose.mini.chains.R
@@ -12,5 +12,26 @@
 #' @export
 
 diagnose.mini.chains <- function(mini.chains) {
+
+    ## Summarising the chain
+    summarised <- summary.MCMCglmm(mini.chains)
+
+    ## Interesting bits?
+    ESS_sol  <- summarised$solutions[, "eff.samp"]
+    ESS_VCVG <- summarised$Gcovariances[, "eff.samp"]
+    ESS_VCVR <- summarised$Rcovariances[, "eff.samp"]
+
+    ## Effective size of the Random terms
+    op <- par(mfrow = c(3,1))
+    hist(ESS_sol, main = "Solution")
+    abline(v = 200)
+
+    hist(ESS_VCVR, main = "Residual terms")
+    abline(v = 200)
+    
+    hist(ESS_VCVG, main = "Random terms")
+    abline(v = 200)
+    par(op)
+
     return("prototype")
 }
